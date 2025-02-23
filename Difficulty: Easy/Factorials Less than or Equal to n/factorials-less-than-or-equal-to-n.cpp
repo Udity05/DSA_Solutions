74fd358b51c5f9d0b3a61aa0440dd673bc483359
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2025-02-23 10:51:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Factorials Less than or Equal to n/factorials-less-than-or-equal-to-n.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<long long> factorialNumbers(long long n) {
+        // Write Your Code here
+     vector<long long> ans;
+        long long int fact = 1;
+        int x = 2;
+
+        // Iterating until factorial is less than or equal to N.
+        while (fact <= n) {
+            // Adding factorial to the answer list.
+            ans.push_back(fact);
+
+            // Calculating next factorial by multiplying with next number.
+            fact = fact * x;
+            x++;
+        }
+        // Returning the list of factorial numbers.
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for (auto num : ans) {
+            cout << num << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
