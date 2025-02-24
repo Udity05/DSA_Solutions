2756d2a51fbf1d6f8268755ca65ebb1f20378ae7
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2025-02-24 20:07:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        int n1=a.size();
+        int n2=b.size();
+        int i=0;
+        int j=0;
+        vector<int> onion;
+        while(i<n1 && j< n2){
+            if(a[i]<=b[j]){
+                if(onion.size() ==0 || onion.back() != a[i]){
+                    onion.push_back(a[i]);
+                }
+                i++;
+            }
+            else{
+             if(onion.size() ==0 || onion.back() != b[j]){
+                    onion.push_back(b[j]);
+                }
+                j++;
+            }
+        }
+        while(j<n2){
+            if(onion.size() ==0 || onion.back() != b[j]){
+                    onion.push_back(b[j]);
+                }
+                j++;
+        }
+        
+        while(i<n1){
+            if(onion.size() ==0 || onion.back() != a[i]){
+                    onion.push_back(a[i]);
+                }
+                i++;
+        }
+        return onion;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
