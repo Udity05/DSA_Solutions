62438c7807f2e00570ad6eab650db0c027b59cce
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2025-06-20 20:29:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.55%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int left = 0, right = m - 1;
-        int top = 0, bottom = n - 1;
-        vector<int> mat;
+        vector<int> ans;
+        if (matrix.empty() || matrix[0].empty()) return ans; 
+        int row = matrix.size(), col = matrix[0].size();
+        int top = 0, bottom = row - 1, left = 0, right = col - 1;
 
-        while (top <= bottom && left <= right) {
-            
-            for (int i = left; i <= right; i++) {
-                mat.push_back(matrix[top][i]);
+        while (left <= right && top <= bottom) {
+            for (int j = left; j <= right; j++) {
+                ans.push_back(matrix[top][j]);
             }
-            top++;
+            top++; 
 
             
             for (int i = top; i <= bottom; i++) {
-                mat.push_back(matrix[i][right]);
+                ans.push_back(matrix[i][right]);
             }
-            right--;
+            right--; 
 
-            
             if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    mat.push_back(matrix[bottom][i]);
+                
+                for (int j = right; j >= left; j--) {
+                    ans.push_back(matrix[bottom][j]);
                 }
-                bottom--;
+                bottom--; 
             }
 
-            
             if (left <= right) {
+                
                 for (int i = bottom; i >= top; i--) {
-                    mat.push_back(matrix[i][left]);
+                    ans.push_back(matrix[i][left]);
                 }
-                left++;
+                left++; 
             }
         }
-
-        return mat;
+        return ans;
     }
 };
