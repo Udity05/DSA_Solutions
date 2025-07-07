5efb7935f0f792c4918b13d774df196ba20303ee
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2025-07-07 19:00:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Greatest of three numbers/greatest-of-three-numbers.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    int greatestOfThree(int A, int B, int C) {
+        // code here
+        int max= Math.max(A,Math.max(B,C));
+        
+        return max;
+    }
+}
