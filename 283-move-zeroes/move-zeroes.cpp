e12af4c7bafdd7f0a0c1f4ec4c91a073d4116702
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2025-08-06 20:37:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (81.27%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int n=nums.size();
-       int j=-1;
-       for(int i =0; i<n;i++){
-        if(nums[i]==0){
-        j=i;
-        break;
+        int n=nums.size();
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[index++]);
+            }
         }
-       }
-       if (j==-1) return ;
-
-       for(int i=j+1; i<n ;i++){
-        if(nums[i]!=0){
-        swap(nums[i],nums[j]);
-        j++;
-        }
-       }
-      for (int num : nums) {
-        cout << num << " ";
-    }
+        
     }
 };
