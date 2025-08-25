c4745e75c41844d8e7816eadd58636d58ae2ec49
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2025-08-25 21:30:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete in a Singly Linked List/delete-in-a-singly-linked-list.cpp
@@ -0,0 +1,38 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* deleteNode(Node* head, int x) {
+        // code here
+         if(x==1){
+             Node *temp=head;
+             head = head->next;
+             delete temp;
+             
+             return head;
+         }
+         
+         x--;
+         Node *curr = head;
+         Node *prev= NULL;
+         
+         while(x--){
+             prev = curr;
+             curr = curr->next;
+         }
+         prev->next = curr->next;
+         delete curr;
+         
+         return head;
+    }
+};
