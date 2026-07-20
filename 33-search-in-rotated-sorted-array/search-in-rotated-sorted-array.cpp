3cc33257681b665b7afcc22b93ed3ddf0e1bd288
Udity Banerjee <116422501+Udity05@users.noreply.github.com>
2026-07-20 19:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (67.94%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,25 +4,24 @@ public:
         int start=0, end=nums.size()-1, mid;
 
         while(start<=end){
-            mid= start+(end-start)/2;
+            mid = start+(end-start)/2;
 
             if(nums[mid]==target)
             return mid;
-
-            if(nums[start]<= nums[mid]){
-                if (nums[start] <= target && target < nums[mid])
+            else if(nums[mid]>=nums[0])
+            {
+                if(nums[start]<=target&&nums[mid]>=target)
                 end=mid-1;
                 else
                 start=mid+1;
             }
-
             else{
-                if (nums[mid] < target && target <= nums[end])
+                if(nums[mid]<target&&nums[end]>=target)
                 start=mid+1;
                 else
                 end=mid-1;
             }
         }
-         return -1;
+        return -1;
     }
 };
